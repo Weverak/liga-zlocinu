14a8001ddf7c113eee87870f7439f747f5e02e8d
„Test“ <jakub.pitner128@seznam.cz>
2021-12-16 09:29:28 +0100

    Splash screen, menu a výběr levelů

---
+++ ligszlocinu/LevelsWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ligszlocinu
+{
+    /// <summary>
+    /// Interakční logika pro LevelsWindow.xaml
+    /// </summary>
+    public partial class LevelsWindow : Window
+    {
+        public LevelsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tutorialButton_Click(object sender, RoutedEventArgs e)
+        {
+            TutorialWindow tutorial = new TutorialWindow();
+            tutorial.Show();
+            Close();
+        }
+    }
+}
---
+++ ligszlocinu/TutorialWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ligszlocinu
+{
+    /// <summary>
+    /// Interakční logika pro TutorialWindow.xaml
+    /// </summary>
+    public partial class TutorialWindow : Window
+    {
+        public TutorialWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            LevelsWindow levels = new LevelsWindow();
+            MessageBoxResult message = MessageBox.Show("Jste si tím jisti?", "Opravdu", MessageBoxButton.YesNo);
+            if (message == MessageBoxResult.Yes)
+            {
+                levels.Show();
+                Close();
+            }
+            else if (message == MessageBoxResult.No)
+                {
+
+            }
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Text.Text="Výborně, ve hře máš vždy dvě možnosti a z nich vybíráš. Tvůj prví případ bude jednoduchý ukradené lízatko :). Máš výhodu že prodavač si toho všimnul a obchod hned zavřel. Tak jedeme";
+            button1.Content = "Jedeme";
+            button2.Content = "OK";
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Text.Text = "Výborně, ve hře máš vždy dvě možnosti a z nich vybíráš. Tvůj prví případ bude jednoduchý ukradené lízatko :). Máš výhodu že prodavač si toho všimnul a obchod hned zavřel. Tak jedeme";
+            button1.Content = "Jedeme";
+            button2.Content = "OK";
+
+        }
+    }
+}
